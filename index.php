0850f37ce966a2a07b35e4e3a7f971a352dd2284
Daniel Popescu <same.dan@gmail.com>
2023-09-01 11:11:05 +0200

    Custom CSS for Plugin

--- index.php
+++ index.php
@@ -17,24 +17,17 @@ class OurWorldFilterPlugin {
   }
 
   function ourMenu() {
-    // add_menu_page(
-    //   'Words to Filter', // Document title
-    //   'Word Filter', // Sidebar text
-    //   'manage_options', // user permissions required
-    //   'ourwordfilter', // slug
-    //   array($this, 'wordFilterPage'), // html for the page
-    //   'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgMCAyMCAyMCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0xMCAyMEMxNS41MjI5IDIwIDIwIDE1LjUyMjkgMjAgMTBDMjAgNC40NzcxNCAxNS41MjI5IDAgMTAgMEM0LjQ3NzE0IDAgMCA0LjQ3NzE0IDAgMTBDMCAxNS41MjI5IDQuNDc3MTQgMjAgMTAgMjBaTTExLjk5IDcuNDQ2NjZMMTAuMDc4MSAxLjU2MjVMOC4xNjYyNiA3LjQ0NjY2SDEuOTc5MjhMNi45ODQ2NSAxMS4wODMzTDUuMDcyNzUgMTYuOTY3NEwxMC4wNzgxIDEzLjMzMDhMMTUuMDgzNSAxNi45Njc0TDEzLjE3MTYgMTEuMDgzM0wxOC4xNzcgNy40NDY2NkgxMS45OVoiIGZpbGw9IiNGRkRGOEQiLz4KPC9zdmc+Cg==', // icon
-    //   100, // number whre menu appears vertically, 1-Top, 100-Down
-    // );
-    add_menu_page(
+    $mainPageHook = add_menu_page(
       'Words to Filter', // Document title
       'Word Filter', // Sidebar text
       'manage_options', // user permissions required
       'ourwordfilter', // slug
       array($this, 'wordFilterPage'), // html for the page
-      plugin_dir_url(__FILE__).'custom.svg', // icon
+      'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgMCAyMCAyMCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0xMCAyMEMxNS41MjI5IDIwIDIwIDE1LjUyMjkgMjAgMTBDMjAgNC40NzcxNCAxNS41MjI5IDAgMTAgMEM0LjQ3NzE0IDAgMCA0LjQ3NzE0IDAgMTBDMCAxNS41MjI5IDQuNDc3MTQgMjAgMTAgMjBaTTExLjk5IDcuNDQ2NjZMMTAuMDc4MSAxLjU2MjVMOC4xNjYyNiA3LjQ0NjY2SDEuOTc5MjhMNi45ODQ2NSAxMS4wODMzTDUuMDcyNzUgMTYuOTY3NEwxMC4wNzgxIDEzLjMzMDhMMTUuMDgzNSAxNi45Njc0TDEzLjE3MTYgMTEuMDgzM0wxOC4xNzcgNy40NDY2NkgxMS45OVoiIGZpbGw9IiNGRkRGOEQiLz4KPC9zdmc+Cg==', // icon
+      // plugin_dir_url(__FILE__).'custom.svg', // icon as original SVG
       100, // number whre menu appears vertically, 1-Top, 100-Down
     );
+    
 
     // FIRST ITEM IN THE SUBMENU that copies the MENU Item and Link
     add_submenu_page( // Used to display the first Item in the SubMenu
@@ -55,17 +48,34 @@ class OurWorldFilterPlugin {
       'word-filter-options',// slug
       array($this, 'optionsSubPage'), // funtion that outputs the html
     );
+
+    // CSS custom for the Menu page
+    add_action("load-{$mainPageHook}", array($this, 'mainPageAssets'));
   }
 
   // html for the MENU page
   function wordFilterPage() { ?>
-    Hello, world
+    <div class="wrap">
+      <h1>Word Filter</h1>
+      <form method="POST">
+          <label for="plugin_words_to_filter"><p>Enter a <strong>comma-separated</strong> list of words to filter from your site's content.</p></label>
+          <div class="word-filter__flex-container">
+            <textarea id="plugin_words_to_filter" name="plugin_words_to_filter" placeholder="bad, mean, awful, horrible"></textarea>
+          </div>
+          <input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes" />
+      </form>
+    </div>
   <?php }
 
   // html for the SUBMENU page
   function optionsSubPage() { ?>
     Hello, world from the Options page
   <?php }
+
+  // CSS custom for the Menu page
+  function mainPageAssets() {
+      wp_enqueue_style('filterAdminCss', plugin_dir_url(__FILE__).'styles.css');
+  }
 
 }
 
